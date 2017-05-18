0dd338b41213e1898e3ed6693c5cc58bf84feae8
Kevin Schoon <kevinschoon@gmail.com>
2017-05-18 16:25:18 -0400

    add new logging options

--- cmd/commands.go
+++ cmd/commands.go
@@ -5,7 +5,9 @@ import (
 	"github.com/jawher/mow.cli"
 	"github.com/vektorlab/gaffer/log"
 	"github.com/vektorlab/gaffer/version"
+	"go.uber.org/zap"
 	"os"
+	"strings"
 )
 
 func maybe(err error) {
@@ -20,10 +22,30 @@ func Run() {
 	app.Spec = "[OPTIONS]"
 	app.Version("version", fmt.Sprintf("%s (%s)", version.Version, version.GitSHA))
 	var (
+		json  = app.BoolOpt("json", false, "enables json log output")
+		level = app.StringOpt("level", "INFO", "specify logging level [ERROR, WARN, INFO, DEBUG]")
 		debug = app.BoolOpt("d debug", false, "output debug information")
 		store = app.StringOpt("s store", "sqlite://gaffer.db", "store configuration pattern")
 	)
 	app.Before = func() {
+		switch strings.ToUpper(*level) {
+		case "ERROR":
+			log.Level.SetLevel(zap.ErrorLevel)
+		case "WARN":
+			log.Level.SetLevel(zap.WarnLevel)
+		case "INFO":
+			log.Level.SetLevel(zap.InfoLevel)
+		case "DEBUG":
+			log.Level.SetLevel(zap.DebugLevel)
+			log.Debug()
+		default:
+			maybe(fmt.Errorf("unsupported logging level %s", *level))
+		}
+		// Enable JSON logging output
+		if *json {
+			log.Json()
+		}
+		//Enable debugging with dev features
 		if *debug {
 			log.Debug()
 		}
--- log/logger.go
+++ log/logger.go
@@ -4,12 +4,30 @@ import (
 	"go.uber.org/zap"
 )
 
-var Log *zap.Logger
+var (
+	Log    *zap.Logger
+	Level  zap.AtomicLevel
+	config zap.Config
+)
 
+// Json toggles JSON logging output
+func Json() {
+	config.Encoding = "json"
+	Log, _ = config.Build()
+}
+
+// Debug toggles development mode
 func Debug() {
-	Log, _ = zap.NewDevelopment()
+	config = zap.NewDevelopmentConfig()
+	config.Level = Level
+	Log, _ = config.Build()
 }
 
 func init() {
-	Log, _ = zap.NewProduction()
+	config = zap.NewProductionConfig()
+	// Default to human friendly console output
+	config.Encoding = "console"
+	Level = zap.NewAtomicLevelAt(zap.InfoLevel)
+	config.Level = Level
+	Log, _ = config.Build()
 }
