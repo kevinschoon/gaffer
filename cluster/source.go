de58afc460cb8b9ceb8d30d3e599e801e89da786
Kevin Schoon <kevinschoon@gmail.com>
2017-06-14 12:43:45 -0300

    add s3 source

--- cluster/source.go
+++ cluster/source.go
@@ -17,6 +17,18 @@ func NewSource(pattern string) (Source, error) {
 		return FileSource{Path: strings.Replace(pattern, "file://", "", -1)}, nil
 	case strings.Contains(pattern, "http"):
 		return NewHTTPSource(pattern)
+	case strings.Contains(pattern, "s3://"):
+		split := strings.Split(strings.Replace(pattern, "s3://", "", -1), "/")
+		var (
+			bucket string
+			key    string
+		)
+		if len(split) < 2 {
+			return nil, fmt.Errorf("bad s3 url: %s", pattern)
+		}
+		bucket = split[0]
+		key = strings.Join(split[1:], "/")
+		return NewS3Source(bucket, key)
 	}
 	return nil, fmt.Errorf("unknown file source: %s", pattern)
 }
---
+++ cluster/source_s3.go
@@ -0,0 +1,71 @@
+package cluster
+
+import (
+	"encoding/json"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"io/ioutil"
+	"os"
+)
+
+const S3ConfigKey = "gaffer.json"
+
+type S3Source struct {
+	uploader   *s3manager.Uploader
+	downloader *s3manager.Downloader
+	bucket     string
+	key        string
+}
+
+func (ss S3Source) Get() (*Config, error) {
+	fp, err := ioutil.TempFile("/tmp", "")
+	if err != nil {
+		return nil, err
+	}
+	defer os.Remove(fp.Name())
+	_, err = ss.downloader.Download(fp, &s3.GetObjectInput{
+		Bucket: &ss.bucket,
+		Key:    &ss.key,
+	})
+	if err != nil {
+		return nil, err
+	}
+	config := &Config{}
+	err = json.NewDecoder(fp).Decode(config)
+	if err != nil {
+		return nil, err
+	}
+	return config, nil
+}
+
+func (ss S3Source) Set(config *Config) error {
+	fp, err := ioutil.TempFile("/tmp", "")
+	if err != nil {
+		return err
+	}
+	defer os.Remove(fp.Name())
+	err = json.NewEncoder(fp).Encode(config)
+	if err != nil {
+		return err
+	}
+	_, err = ss.uploader.Upload(&s3manager.UploadInput{
+		Bucket: &ss.bucket,
+		Key:    &ss.key,
+		Body:   fp,
+	})
+	return err
+}
+
+func NewS3Source(bucket, key string) (*S3Source, error) {
+	sess, err := session.NewSession()
+	if err != nil {
+		return nil, err
+	}
+	return &S3Source{
+		uploader:   s3manager.NewUploader(sess),
+		downloader: s3manager.NewDownloader(sess),
+		bucket:     bucket,
+		key:        key,
+	}, nil
+}
